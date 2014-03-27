0c472d9caf02a3389c623c540f8f2bca82669f94
root <root@localhost>
2014-03-27 15:06:38 +0530

    commiting hello srina

--- practiceandroid/src/com/india/webguru/practiceandroid/MainActivity.java
+++ practiceandroid/src/com/india/webguru/practiceandroid/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends FragmentActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("-- Hello Srina?");
     }
 
     
